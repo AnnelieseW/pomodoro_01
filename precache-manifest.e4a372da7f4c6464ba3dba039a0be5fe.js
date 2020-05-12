dcd1e21086f34a5c13706479aeb63784908e9563
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-05-11 20:28:42 -0400

    Updates

---
+++ precache-manifest.e4a372da7f4c6464ba3dba039a0be5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e98efd18e559a3b99cf077cb3f32757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa7c1461d2ce44a4b26",
+    "url": "/static/css/main.f7a212d3.chunk.css"
+  },
+  {
+    "revision": "58f60485154df77aae1d",
+    "url": "/static/js/2.a33f1a70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a33f1a70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa7c1461d2ce44a4b26",
+    "url": "/static/js/main.215888d3.chunk.js"
+  },
+  {
+    "revision": "7a34ee77d258ae8162d3",
+    "url": "/static/js/runtime-main.8383b91c.js"
+  }
+]);
